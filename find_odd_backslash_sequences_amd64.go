e558c47eaf44ebaecce1c8cd2b10f112ffb3a0bd
frankw <fwessels@xs4all.nl>
2019-09-12 13:41:50 -0700

    Initial port of find_odd_backslash_sequences

---
+++ find_odd_backslash_sequences_amd64.go
@@ -0,0 +1,15 @@
+//+build !noasm
+//+build !appengine
+
+package simdjson
+
+import (
+	"unsafe"
+)
+
+//go:noescape
+func _find_odd_backslash_sequences(p1, p2, p3 unsafe.Pointer) (result uint64)
+
+func find_odd_backslash_sequences(buf []byte, prev_iter_ends_odd_backslash *uint64) uint64 {
+	return _find_odd_backslash_sequences(unsafe.Pointer(&buf[0]), unsafe.Pointer(&buf[32]), unsafe.Pointer(prev_iter_ends_odd_backslash))
+}
