e052989bf67a6bc3a6125b7a8dfb7349cb226b2f
frankw <fwessels@xs4all.nl>
2019-09-16 11:31:30 -0700

    Add finalize_structurals code

---
+++ finalize_structurals_amd64.go
@@ -0,0 +1,15 @@
+//+build !noasm
+//+build !appengine
+
+package simdjson
+
+import (
+	"unsafe"
+)
+
+//go:noescape
+func _finalize_structurals(structurals_in, whitespace, quote_mask, quote_bits uint64, prev_iter_ends_pseudo_pred unsafe.Pointer) (structurals uint64)
+
+func finalize_structurals(structurals, whitespace, quote_mask, quote_bits uint64, prev_iter_ends_pseudo_pred *uint64) uint64 {
+	return _finalize_structurals(structurals, whitespace, quote_mask, quote_bits, unsafe.Pointer(prev_iter_ends_pseudo_pred))
+}
