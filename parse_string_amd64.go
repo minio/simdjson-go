9fd65339873f290359b77ba4b50ba4ad8117a2f9
frankw <fwessels@xs4all.nl>
2019-09-20 16:29:04 -0700

    Improved wrapper around parse_string

--- parse_string_amd64.go
+++ parse_string_amd64.go
@@ -11,20 +11,21 @@ import (
 //go:noescape
 func _parse_string(src, dst, pcurrent_string_buf_loc unsafe.Pointer)
 
-func parse_string_simd(buf []byte, stringbuf *[]byte) uintptr {
+func parse_string_simd(buf []byte, stringbuf *[]byte) int {
 
 	sh := (*reflect.SliceHeader)(unsafe.Pointer(stringbuf))
 
+	string_buf_loc :=  uintptr(unsafe.Pointer(sh.Data)) + uintptr(sh.Len)
 	src := uintptr(unsafe.Pointer(&buf[0])) + 1 // const uint8_t *src = &buf[offset + 1];
-	dst := unsafe.Pointer(sh.Data + 4)          // uint8_t *dst = pj.current_string_buf_loc + sizeof(uint32_t);
-	string_buf_loc := unsafe.Pointer(sh.Data)
+	dst := string_buf_loc + 4                   // uint8_t *dst = pj.current_string_buf_loc + sizeof(uint32_t);
 
-	_parse_string(unsafe.Pointer(src), dst, unsafe.Pointer(&string_buf_loc))
+	_parse_string(unsafe.Pointer(src), unsafe.Pointer(dst), unsafe.Pointer(&string_buf_loc))
 
-	//if int(size) >= sh.Cap {
-	//	panic("Memory corruption -- written beyond slice capacity -- expected capacity to be larger than max values written")
-	//}
-	//sh.Len = int(size)
+	written := int(uintptr(string_buf_loc) - (dst - 4))
+	if sh.Len + written >= sh.Cap {
+		panic("Memory corruption -- written beyond slice capacity -- expected capacity to be larger than max values written")
+	}
+	sh.Len += written
 
-	return uintptr(string_buf_loc) - sh.Data
+	return written
 }
--- parse_string_test.go
+++ parse_string_test.go
@@ -8,13 +8,10 @@ import (
 
 func TestParseString(t *testing.T) {
 
-	const str = "label"
+	stringbuf := make([]byte, 0, 256)
 
-	buf := make([]byte, 1024)
-	copy(buf, fmt.Sprintf(`"%s":`, str))
-	stringbuf := make([]byte, 256)
-
-	size := parse_string_simd(buf, &stringbuf)
+	const str = "key"
+	size := parse_string_simd([]byte(fmt.Sprintf(`"%s":`, str)), &stringbuf)
 
 	// First four bytes are size
 	length := int(binary.LittleEndian.Uint32(stringbuf[0:4]))
@@ -29,8 +26,27 @@ func TestParseString(t *testing.T) {
 	if stringbuf[4+length] != 0 {
 		t.Errorf("TestParseString: got: 0x%x want: 0x0", stringbuf[4+length])
 	}
+	if size != 4 + length + 1 {
+		t.Errorf("TestParseString: got: %d want: %d", size, 4 + length + 1)
+	}
 
-	if size != uintptr(4+length+1) {
-		t.Errorf("TestParseString: got: %d want: %d", size, uintptr(4+length+1))
+	const str2 = "value"
+	size2 := parse_string_simd([]byte(fmt.Sprintf(`"%s":`, str2)), &stringbuf)
+
+	// First four bytes are size
+	length = int(binary.LittleEndian.Uint32(stringbuf[size:size+4]))
+	if length != len(str2) {
+		t.Errorf("TestParseString: got: %d want: %d", length, len(str2))
+	}
+	// Then comes value of string
+	if string(stringbuf[size+4:size+4+length]) != str2 {
+		t.Errorf("TestParseString: got: %s want: %s", string(stringbuf[size+4:size+4+length]), str2)
+	}
+	// Followed by NULL-character
+	if stringbuf[size+4+length] != 0 {
+		t.Errorf("TestParseString: got: 0x%x want: 0x0", stringbuf[size+4+length])
+	}
+	if size2 != 4 + length + 1 {
+		t.Errorf("TestParseString: got: %d want: %d", size2, 4 + length + 1)
 	}
 }
