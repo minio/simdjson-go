e102c776f1533d434ce26c947e4cd218e68ecf13
frankw <fwessels@xs4all.nl>
2019-10-08 15:45:43 -0700

    Fix parse_number for negative numbers

--- parse_number_amd64.go
+++ parse_number_amd64.go
@@ -10,11 +10,16 @@ import (
 //go:noescape
 func _parse_number(buf unsafe.Pointer, offset, found_minus uint64, is_double, resultDouble, resultInt64 unsafe.Pointer) (success uint64)
 
-func parse_number_simd(buf []byte) (success, is_double bool, d float64, i int64) {
+func parse_number_simd(buf []byte, found_minus bool) (success, is_double bool, d float64, i int64) {
 
 	src := uintptr(unsafe.Pointer(&buf[0]))
 
-	success = _parse_number(unsafe.Pointer(src), 0, 0, unsafe.Pointer(&is_double), unsafe.Pointer(&d), unsafe.Pointer(&i)) != 0
+	fm := uint64(0)
+	if found_minus {
+		fm = 1
+	}
+
+	success = _parse_number(unsafe.Pointer(src), 0, fm, unsafe.Pointer(&is_double), unsafe.Pointer(&d), unsafe.Pointer(&i)) != 0
 
 	return
 }
--- parse_number_test.go
+++ parse_number_test.go
@@ -2,8 +2,13 @@ package simdjson
 
 import (
 	"fmt"
+	"math"
 	"testing"
 )
+
+func closeEnough(d1, d2 float64) (ce bool) {
+	return math.Abs(d1 - d2) / (0.5*(d1 + d2)) < 1e-12
+}
 
 func TestParseNumber(t *testing.T) {
 
@@ -14,17 +19,22 @@ func TestParseNumber(t *testing.T) {
 		expectedI int64
 	}{
 		{"1", false, 0.0, 1},
+		{"-1", false, 0.0, -1},
 		{"1.0", true, 1.0, 0},
 		{"1234567890", false, 0.0, 1234567890},
 		{"9876.543210", true, 9876.543210, 0},
-	/*	{ "0.123456789e-12", true, 1.23456789e-13, 0}, */
-	/*	{ "1.234567890E+34", true, 1.234567890E+34, 0}, */
-	/*	{ "23456789012E66", true, 23456789012E66, 0}, */
-	/*	{"-9876.543210", true, -9876.543210, 0}, // fails */
+		{ "0.123456789e-12", true, 1.23456789e-13, 0},
+		{ "1.234567890E+34", true, 1.234567890E+34, 0},
+		{ "23456789012E66", true, 23456789012E66, 0},
+		{"-9876.543210", true, -9876.543210, 0}, // fails
 	}
 
 	for _, tc := range testCases {
-		succes, is_double, d, i := parse_number_simd([]byte(fmt.Sprintf(`%s:`, tc.input)))
+		found_minus := false
+		if tc.input[0] == '-' {
+			found_minus = true
+		}
+		succes, is_double, d, i := parse_number_simd([]byte(fmt.Sprintf(`%s:`, tc.input)), found_minus)
 		if !succes {
 			t.Errorf("TestParseNumber: got: %v want: %v", succes, true)
 		}
@@ -32,7 +42,7 @@ func TestParseNumber(t *testing.T) {
 			t.Errorf("TestParseNumber: got: %v want: %v", is_double, tc.is_double)
 		}
 		if is_double {
-			if d != tc.expectedD {
+			if !closeEnough(d, tc.expectedD) {
 				t.Errorf("TestParseNumber: got: %g want: %g", d, tc.expectedD)
 			}
 		} else {
