1d00bfe93dff31cf9d279ef8a0c3a03cae9d584c
frankw <fwessels@xs4all.nl>
2019-11-05 15:09:16 -0800

    Prepare split for integration in Stage 1

--- flatten_bits_amd64.s
+++ flatten_bits_amd64.s
@@ -14,6 +14,13 @@ TEXT ·_flatten_bits_incremental(SB), $0-32
     MOVQ pcarried+24(FP), R11
     MOVQ (SI), INDEX
     MOVQ (R11), CARRIED
+    CALL ·__flatten_bits_incremental(SB)
+    MOVQ   CARRIED, (R11)
+    MOVQ   INDEX, (SI)
+    RET
+
+
+TEXT ·__flatten_bits_incremental(SB), $0
     XORQ SHIFTS, SHIFTS
 
     // First iteration takes CARRIED into account
@@ -47,6 +54,4 @@ done:
     MOVQ   $64, R9
     SUBQ   SHIFTS, R9
     ADDQ   R9, CARRIED    // CARRIED += 64 - shifts (remaining empty bits to carry over to next call)
-    MOVQ   CARRIED, (R11)
-    MOVQ   INDEX, (SI)
     RET
