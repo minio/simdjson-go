33728fa168a78f9660ba4fe76d7907721c5da09c
frankw <fwessels@xs4all.nl>
2019-11-05 17:01:33 -0800

    Fix typo

--- find_structural_bits_amd64.go
+++ find_structural_bits_amd64.go
@@ -9,7 +9,7 @@ import (
 
 //go:noescape
 func _find_structural_bits(p1, p3 unsafe.Pointer,                                         /* for: find_odd_backslash_sequences()    */
-						   prev_iter_inside_quote, quote_bits, error_mask unsafe.Pointer, /* for: find_quoute_mask_and_bits()       */
+						   prev_iter_inside_quote, quote_bits, error_mask unsafe.Pointer, /* for: find_quote_mask_and_bits()        */
 						   whitespace, structurals_in unsafe.Pointer,                     /* for: find_whitespace_and_structurals() */
 						   prev_iter_ends_pseudo_pred unsafe.Pointer,                     /* for: finalize_structurals()            */
 						   ) (structurals uint64)
