f3d3a5f1e58ce9e3184ec9132ce54e4658a78f82
frankw <fwessels@xs4all.nl>
2019-11-26 13:00:10 -0800

    Correct test Tape formats form C version for initial root element that points to one position higher

--- parsed_json_test.go
+++ parsed_json_test.go
@@ -3,6 +3,7 @@ package simdjson
 import (
 	"bytes"
 	"encoding/json"
+	"encoding/binary"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -25,6 +26,9 @@ func loadCompressed(t tester, file string) (tape, sb, ref []byte) {
 		t.Fatal(err)
 	}
 	tap, err = dec.DecodeAll(tap, nil)
+	// Our end-of-root has been incremented by one (past last element) for quick skipping of ndjson
+	// So correct the initial root element to point to one position higher
+	binary.LittleEndian.PutUint64(tap, binary.LittleEndian.Uint64(tap) + 1)
 	if err != nil {
 		t.Fatal(err)
 	}
