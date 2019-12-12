a44a3c42d8c4a1d0b10524507b02616a59cc73b0
frankw <fwessels@xs4all.nl>
2019-12-12 09:42:33 -0800

    Integrate parse_string_validate_only into parse_string

--- parse_string_amd64.go
+++ parse_string_amd64.go
@@ -9,7 +9,21 @@ import (
 )
 
 //go:noescape
+func _parse_string_validate_only(src, str_length, dst_length unsafe.Pointer) (result uint64)
+
+//go:noescape
 func _parse_string(src, dst, pcurrent_string_buf_loc unsafe.Pointer) (res uint64)
+
+func parse_string_simd_validate_only(buf []byte, dst_length *uint64, need_copy *bool) bool {
+
+	src := uintptr(unsafe.Pointer(&buf[0])) + 1 // const uint8_t *src = &buf[offset + 1];
+	src_length := uint64(0)
+
+	success := _parse_string_validate_only(unsafe.Pointer(src), unsafe.Pointer(&src_length), unsafe.Pointer(dst_length))
+
+	*need_copy = src_length != *dst_length
+	return success != 0
+}
 
 func parse_string_simd(buf []byte, stringbuf *[]byte) bool {
 
