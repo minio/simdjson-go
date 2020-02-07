6ae7d5249122f0018ff661258b0f0e699eeda6a3
frankw <fwessels@xs4all.nl>
2020-02-06 17:19:39 -0800

    Example code

---
+++ examples/simdjson_example.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"github.com/fwessels/simdjson-go"
+	"io/ioutil"
+	"log"
+)
+
+func printKey(iter simdjson.Iter, key string) (err error) {
+
+	obj, tmp, elem := &simdjson.Object{}, &simdjson.Iter{},  simdjson.Element{}
+
+	for {
+		typ := iter.Advance()
+
+		switch typ {
+		case simdjson.TypeRoot:
+			if typ, tmp, err = iter.Root(tmp); err != nil {
+				return
+			}
+
+			if typ == simdjson.TypeObject {
+				if obj, err = tmp.Object(obj); err != nil {
+					return
+				}
+
+				e := obj.FindKey(key, &elem)
+				if e != nil && elem.Type == simdjson.TypeString {
+					v, _ := elem.Iter.StringBytes()
+					fmt.Println(string(v))
+				}
+			}
+
+		default:
+			return
+		}
+	}
+}
+
+func main() {
+
+	msg, err := ioutil.ReadFile("parking-citations.json")
+	if err != nil {
+		log.Fatalf("Failed to load file: %v", err)
+	}
+
+	parsed, err := simdjson.ParseND(msg, nil)
+	if err != nil {
+		log.Fatalf("Failed to parse JSON: %v", err)
+	}
+
+	printKey(parsed.Iter(), "Make")
+}
