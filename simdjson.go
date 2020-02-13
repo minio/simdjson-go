e49ab17ab74699d39571970b8c0bd9b83ae1f48e
Klaus Post <klauspost@gmail.com>
2020-02-12 17:12:28 -0800

    Fix racy input buffer (#4)
    Wait until we get the buffer back before putting it back into the pool.

--- simdjson.go
+++ simdjson.go
@@ -158,19 +158,22 @@ func ParseNDStream(r io.Reader, res chan<- Stream, reuse <-chan *ParsedJson) {
 				err = err2
 			}
 
-			trimmed := bytes.TrimSpace(tmp)
-			if len(trimmed) > 0 {
+			if len(tmp) > 0 {
 				result := make(chan Stream, 0)
 				queue <- result
 				go func() {
-					defer tmpPool.Put(tmp)
 					var pj internalParsedJson
 					select {
 					case v := <-reuse:
+						if cap(v.Message) >= tmpSize+1024 {
+							tmpPool.Put(v.Message)
+							v.Message = nil
+						}
 						pj.ParsedJson = *v
+
 					default:
 					}
-					parseErr := pj.parseMessageNdjson(trimmed)
+					parseErr := pj.parseMessageNdjson(tmp)
 					if parseErr != nil {
 						result <- Stream{
 							Value: nil,
