minio/simdjson-go

commit 68d6648cff074aa7330690f4860d5cf15d808d36

Remove parsing allocs (#55)
* Re-use the channel between runs.
* Fix temp slice escaping in findStructuralIndices.

Cleanup: Don't send params already in internalParsedJson.

```
benchmark                                  old ns/op     new ns/op     delta
BenchmarkParseSmall/copy-32                1095          753           -31.20%
BenchmarkParseSmall/nocopy-32              1014          684           -32.53%
BenchmarkParseMedium/copy-32               4359          3981          -8.67%
BenchmarkParseMedium/nocopy-32             3672          3203          -12.77%
BenchmarkParseLarge/copy-32                57643         56759         -1.53%
BenchmarkParseLarge/nocopy-32              47546         45712         -3.86%
BenchmarkParseApache_builds/copy-32        140467        131633        -6.29%
BenchmarkParseApache_builds/nocopy-32      105274        96613         -8.23%
BenchmarkParseCanada/copy-32               11454566      12444019      +8.64%
BenchmarkParseCanada/nocopy-32             11440363      12444615      +8.78%
BenchmarkParseCitm_catalog/copy-32         1499697       1388706       -7.40%
BenchmarkParseCitm_catalog/nocopy-32       1209008       1176916       -2.65%
BenchmarkParseGithub_events/copy-32        71298         69322         -2.77%
BenchmarkParseGithub_events/nocopy-32      57038         54787         -3.95%
BenchmarkParseGsoc_2018/copy-32            1281316       1272766       -0.67%
BenchmarkParseGsoc_2018/nocopy-32          1026481       1011825       -1.43%
BenchmarkParseInstruments/copy-32          302662        289939        -4.20%
BenchmarkParseInstruments/nocopy-32        257394        244188        -5.13%
BenchmarkParseMarine_ik/copy-32            12879480      13697518      +6.35%
BenchmarkParseMarine_ik/nocopy-32          12551823      13322868      +6.14%
BenchmarkParseMesh/copy-32                 3825973       4120152       +7.69%

parse_json_amd64.go
@@ -28,7 +28,7 @@ import (
 func (pj *internalParsedJson) initialize(size int) {
 	// Estimate the tape size to be about 15% of the length of the JSON message
 	avgTapeSize := size * 15 / 100
-	if cap(pj.Tape) < avgTapeSize {
+	if cap(pj.Tape) <= avgTapeSize {
 		pj.Tape = make([]uint64, 0, avgTapeSize)
 	}
 	pj.Tape = pj.Tape[:0]
@@ -46,6 +46,7 @@ func (pj *internalParsedJson) initialize(size int) {
 		pj.containingScopeOffset = make([]uint64, 0, maxdepth)
 	}
 	pj.containingScopeOffset = pj.containingScopeOffset[:0]
+	pj.indexesChan = indexChan{}
 }
 
 func (pj *internalParsedJson) parseMessage(msg []byte, ndjson bool) (err error) {
@@ -63,7 +64,9 @@ func (pj *internalParsedJson) parseMessage(msg []byte, ndjson bool) (err error) 
 	// Make the capacity of the channel smaller than the number of slots.
 	// This way the sender will automatically block until the consumer
 	// has finished the slot it is working on.
-	pj.indexChans = make(chan indexChan, indexSlots-2)
+	if pj.indexChans == nil {
+		pj.indexChans = make(chan indexChan, indexSlots-2)
+	}
 	pj.buffersOffset = ^uint64(0)
 
 	var errStage1 error
@@ -74,29 +77,43 @@ func (pj *internalParsedJson) parseMessage(msg []byte, ndjson bool) (err error) 
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			if !pj.unifiedMachine(pj.Message) {
+			if !pj.unifiedMachine() {
 				err = errors.New("Bad parsing while executing stage 2")
-				// drain the channel until empty
-				for range pj.indexChans {
+				// Keep consuming...
+				for idx := range pj.indexChans {
+					if idx.index == -1 {
+						break
+					}
 				}
 			}
 		}()
-		if !pj.findStructuralIndices(pj.Message) {
+		if !pj.findStructuralIndices() {
 			errStage1 = errors.New("Failed to find all structural indices for stage 1")
 		}
 		wg.Wait()
 	} else {
-		if !pj.findStructuralIndices(pj.Message) {
+		if !pj.findStructuralIndices() {
 			// drain the channel until empty
-			for range pj.indexChans {
+			for idx := range pj.indexChans {
+				if idx.index == -1 {
+					break
+				}
 			}
 			return errors.New("Failed to find all structural indices for stage 1")
 		}
-		if !pj.unifiedMachine(pj.Message) {
+		if !pj.unifiedMachine() {
 			// drain the channel until empty
-			for range pj.indexChans {
+			for {
+				select {
+				case idx := <-pj.indexChans:
+					if idx.index == -1 {
+						return errors.New("Bad parsing while executing stage 2")
+					}
+					// Already drained.
+				default:
+					return errors.New("Bad parsing while executing stage 2")
+				}
 			}
-			return errors.New("Bad parsing while executing stage 2")
 		}
 		return nil
 	}

benchmarks_test.go
@@ -58,26 +58,42 @@ func benchmarkFromFile(b *testing.B, filename string) {
 			}
 		}
 	})
+	b.Run("nocopy-par", func(b *testing.B) {
+		b.RunParallel(func(pb *testing.PB) {
+			pj := &ParsedJson{}
+			b.SetBytes(int64(len(msg)))
+			b.ReportAllocs()
+			b.ResetTimer()
+			for pb.Next() {
+				// Reset tape
+				var err error
+				pj, err = Parse(msg, pj, WithCopyStrings(false))
+				if err != nil {
+					b.Fatal(err)
+				}
+			}
+		})
+	})
 
 }
 
-func BenchmarkSmall(b *testing.B)          { benchmarkFromFile(b, "payload-small") }
-func BenchmarkMedium(b *testing.B)         { benchmarkFromFile(b, "payload-medium") }
-func BenchmarkLarge(b *testing.B)          { benchmarkFromFile(b, "payload-large") }
-func BenchmarkApache_builds(b *testing.B)  { benchmarkFromFile(b, "apache_builds") }
-func BenchmarkCanada(b *testing.B)         { benchmarkFromFile(b, "canada") }
-func BenchmarkCitm_catalog(b *testing.B)   { benchmarkFromFile(b, "citm_catalog") }
-func BenchmarkGithub_events(b *testing.B)  { benchmarkFromFile(b, "github_events") }
-func BenchmarkGsoc_2018(b *testing.B)      { benchmarkFromFile(b, "gsoc-2018") }
-func BenchmarkInstruments(b *testing.B)    { benchmarkFromFile(b, "instruments") }
-func BenchmarkMarine_ik(b *testing.B)      { benchmarkFromFile(b, "marine_ik") }
-func BenchmarkMesh(b *testing.B)           { benchmarkFromFile(b, "mesh") }
-func BenchmarkMesh_pretty(b *testing.B)    { benchmarkFromFile(b, "mesh.pretty") }
-func BenchmarkNumbers(b *testing.B)        { benchmarkFromFile(b, "numbers") }
-func BenchmarkRandom(b *testing.B)         { benchmarkFromFile(b, "random") }
-func BenchmarkTwitter(b *testing.B)        { benchmarkFromFile(b, "twitter") }
-func BenchmarkTwitterEscaped(b *testing.B) { benchmarkFromFile(b, "twitterescaped") }
-func BenchmarkUpdate_center(b *testing.B)  { benchmarkFromFile(b, "update-center") }
+func BenchmarkParseSmall(b *testing.B)          { benchmarkFromFile(b, "payload-small") }
+func BenchmarkParseMedium(b *testing.B)         { benchmarkFromFile(b, "payload-medium") }
+func BenchmarkParseLarge(b *testing.B)          { benchmarkFromFile(b, "payload-large") }
+func BenchmarkParseApache_builds(b *testing.B)  { benchmarkFromFile(b, "apache_builds") }
+func BenchmarkParseCanada(b *testing.B)         { benchmarkFromFile(b, "canada") }
+func BenchmarkParseCitm_catalog(b *testing.B)   { benchmarkFromFile(b, "citm_catalog") }
+func BenchmarkParseGithub_events(b *testing.B)  { benchmarkFromFile(b, "github_events") }
+func BenchmarkParseGsoc_2018(b *testing.B)      { benchmarkFromFile(b, "gsoc-2018") }
+func BenchmarkParseInstruments(b *testing.B)    { benchmarkFromFile(b, "instruments") }
+func BenchmarkParseMarine_ik(b *testing.B)      { benchmarkFromFile(b, "marine_ik") }
+func BenchmarkParseMesh(b *testing.B)           { benchmarkFromFile(b, "mesh") }
+func BenchmarkParseMesh_pretty(b *testing.B)    { benchmarkFromFile(b, "mesh.pretty") }
+func BenchmarkParseNumbers(b *testing.B)        { benchmarkFromFile(b, "numbers") }
+func BenchmarkParseRandom(b *testing.B)         { benchmarkFromFile(b, "random") }
+func BenchmarkParseTwitter(b *testing.B)        { benchmarkFromFile(b, "twitter") }
+func BenchmarkParseTwitterEscaped(b *testing.B) { benchmarkFromFile(b, "twitterescaped") }
+func BenchmarkParseUpdate_center(b *testing.B)  { benchmarkFromFile(b, "update-center") }
 
 func benchmarkJsoniter(b *testing.B, filename string) {
 

parse_json_amd64_test.go
@@ -104,10 +104,11 @@ func BenchmarkNdjsonStage1(b *testing.B) {
 	b.ReportAllocs()
 	b.ResetTimer()
 
+	pj.Message = ndjson
 	for i := 0; i < b.N; i++ {
 		// Create new channel (large enough so we won't block)
 		pj.indexChans = make(chan indexChan, 128*10240)
-		pj.findStructuralIndices([]byte(ndjson))
+		pj.findStructuralIndices()
 	}
 }
 

stage1_find_marks_amd64.go
@@ -38,13 +38,9 @@ func jsonMarkup(b byte) bool {
 	return jsonMarkupTable[b]
 }
 
-func (pj *internalParsedJson) findStructuralIndices(buf []byte) bool {
-
-	f := find_structural_bits_in_slice
-	if cpuid.CPU.Has(cpuid.AVX512F) {
-		f = find_structural_bits_in_slice_avx512
-	}
-
+func (pj *internalParsedJson) findStructuralIndices() bool {
+	avx512 := cpuid.CPU.Has(cpuid.AVX512F)
+	buf := pj.Message
 	// persistent state across loop
 	// does the last iteration end with an odd-length sequence of backslashes?
 	// either 0 or 1, but a 64-bit value
@@ -84,10 +80,18 @@ func (pj *internalParsedJson) findStructuralIndices(buf []byte) bool {
 			stripped_index = ^uint64(0)
 		}
 
-		processed := f(buf[:len(buf) & ^63], &prev_iter_ends_odd_backslash,
-			&prev_iter_inside_quote, &error_mask,
-			&prev_iter_ends_pseudo_pred,
-			index.indexes, &index.length, &carried, &position, pj.ndjson)
+		var processed uint64
+		if avx512 {
+			processed = find_structural_bits_in_slice_avx512(buf[:len(buf) & ^63], &prev_iter_ends_odd_backslash,
+				&prev_iter_inside_quote, &error_mask,
+				&prev_iter_ends_pseudo_pred,
+				index.indexes, &index.length, &carried, &position, pj.ndjson)
+		} else {
+			processed = find_structural_bits_in_slice(buf[:len(buf) & ^63], &prev_iter_ends_odd_backslash,
+				&prev_iter_inside_quote, &error_mask,
+				&prev_iter_ends_pseudo_pred,
+				index.indexes, &index.length, &carried, &position, pj.ndjson)
+		}
 
 		// Check if we have at most a single iteration of 64 bytes left, tag on to previous invocation
 		if uint64(len(buf))-processed <= 64 {
@@ -95,10 +99,17 @@ func (pj *internalParsedJson) findStructuralIndices(buf []byte) bool {
 			paddedBuf := [128]byte{}
 			copy(paddedBuf[:], buf[processed:])
 			paddedBytes := uint64(len(buf)) - processed
-			processed += f(paddedBuf[:paddedBytes], &prev_iter_ends_odd_backslash,
-				&prev_iter_inside_quote, &error_mask,
-				&prev_iter_ends_pseudo_pred,
-				index.indexes, &index.length, &carried, &position, pj.ndjson)
+			if avx512 {
+				processed += find_structural_bits_in_slice_avx512(paddedBuf[:paddedBytes], &prev_iter_ends_odd_backslash,
+					&prev_iter_inside_quote, &error_mask,
+					&prev_iter_ends_pseudo_pred,
+					index.indexes, &index.length, &carried, &position, pj.ndjson)
+			} else {
+				processed += find_structural_bits_in_slice(paddedBuf[:paddedBytes], &prev_iter_ends_odd_backslash,
+					&prev_iter_inside_quote, &error_mask,
+					&prev_iter_ends_pseudo_pred,
+					index.indexes, &index.length, &carried, &position, pj.ndjson)
+			}
 		}
 
 		if index.length == 0 { // No structural chars found, so error out
@@ -130,7 +141,7 @@ func (pj *internalParsedJson) findStructuralIndices(buf []byte) bool {
 		buf = buf[processed:]
 		position -= processed
 	}
-	close(pj.indexChans)
+	pj.indexChans <- indexChan{index: -1}
 
 	// a valid JSON file cannot have zero structural indexes - we should have found something
 	return error_mask == 0 && indexTotal > 0

stage1_find_marks_amd64_test.go
@@ -144,10 +144,14 @@ func TestFindStructuralIndices(t *testing.T) {
 	pj.indexChans = make(chan indexChan, 16)
 
 	// No need to spawn go-routine since the channel is large enough
-	pj.findStructuralIndices([]byte(demo_json))
+	pj.Message = []byte(demo_json)
+	pj.findStructuralIndices()
 
 	ipos, pos := 0, ^uint64(0)
 	for ic := range pj.indexChans {
+		if ic.index == -1 {
+			break
+		}
 		for j := 0; j < ic.length; j++ {
 			pos += uint64((*ic.indexes)[j])
 			result := fmt.Sprintf("%s%s", strings.Repeat(" ", int(pos)), demo_json[pos:])
@@ -171,10 +175,10 @@ func BenchmarkStage1(b *testing.B) {
 	b.ResetTimer()
 
 	pj := internalParsedJson{}
-
+	pj.Message = msg
 	for i := 0; i < b.N; i++ {
 		// Create new channel (large enough so we won't block)
 		pj.indexChans = make(chan indexChan, 128)
-		pj.findStructuralIndices([]byte(msg))
+		pj.findStructuralIndices()
 	}
 }

stage2_build_tape_amd64.go
@@ -33,10 +33,9 @@ const retAddressArrayConst = 3
 
 func updateChar(pj *internalParsedJson, idx_in uint64) (done bool, idx uint64) {
 	if pj.indexesChan.index >= pj.indexesChan.length {
-		var ok bool
-		pj.indexesChan, ok = <-pj.indexChans // Get next element from channel
-		if !ok {
-			done = true // return done if channel closed
+		pj.indexesChan = <-pj.indexChans // Get next element from channel
+		done = pj.indexesChan.index == -1
+		if done {
 			return
 		}
 	}
@@ -158,8 +157,8 @@ func isValidNullAtom(buf []byte) bool {
 	return false
 }
 
-func (pj *internalParsedJson) unifiedMachine(buf []byte) bool {
-
+func (pj *internalParsedJson) unifiedMachine() bool {
+	buf := pj.Message
 	const addOneForRoot = 1
 
 	done := false

commit ca86f7042b3e2aed73129ca96f174a3cd715237e

Fix incorrect reallocation
Regression from #55

parse_json_amd64.go
@@ -28,7 +28,7 @@ import (
 func (pj *internalParsedJson) initialize(size int) {
 	// Estimate the tape size to be about 15% of the length of the JSON message
 	avgTapeSize := size * 15 / 100
-	if cap(pj.Tape) <= avgTapeSize {
+	if cap(pj.Tape) < avgTapeSize {
 		pj.Tape = make([]uint64, 0, avgTapeSize)
 	}
 	pj.Tape = pj.Tape[:0]